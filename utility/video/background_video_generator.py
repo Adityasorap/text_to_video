4e173af40210533cc8398b54cd79206154de2a2f
Adityasorap <114155463+Adityasorap@users.noreply.github.com>
2024-10-24 11:10:17 +0530

    Update background_video_generator.py

--- utility/video/background_video_generator.py
+++ utility/video/background_video_generator.py
@@ -1,8 +1,6 @@
 import os
 import requests
-from utility.utils import log_response, LOG_TYPE_BING
-from utility.utils import log_response, LOG_TYPE_BING_VIDEO
-
+from utility.utils import log_response, LOG_TYPE_BING, LOG_TYPE_BING_VIDEO
 
 BING_API_KEY = os.environ.get('BING_KEY')
 
@@ -19,14 +17,21 @@ def search_videos(query_string, orientation_landscape=True):
     }
 
     response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        print(f"Error fetching videos: {response.status_code} - {response.text}")
+        return {}
+
     json_data = response.json()
     log_response(LOG_TYPE_BING, query_string, json_data)
-
     return json_data
 
 def getBestVideo(query_string, orientation_landscape=True, used_vids=[]):
     vids = search_videos(query_string, orientation_landscape)
-    videos = vids.get('value', [])  # Extract the videos list from JSON
+    videos = vids.get('value', [])
+    
+    if not videos:
+        print("No videos found for the query:", query_string)
+        return None
 
     # Filter based on orientation
     if orientation_landscape:
@@ -43,7 +48,7 @@ def getBestVideo(query_string, orientation_landscape=True, used_vids=[]):
         if video_link and not (video_link.split('.hd')[0] in used_vids):
             return video_link
 
-    print("NO LINKS found for this round of search with query :", query_string)
+    print("NO LINKS found for this round of search with query:", query_string)
     return None
 
 def generate_video_url(timed_video_searches, video_server):
@@ -51,7 +56,7 @@ def generate_video_url(timed_video_searches, video_server):
     if video_server == "bing":
         used_links = []
         for (t1, t2), search_terms in timed_video_searches:
-            url = ""
+            url = None
             for query in search_terms:
                 url = getBestVideo(query, orientation_landscape=True, used_vids=used_links)
                 if url:
@@ -62,3 +67,4 @@ def generate_video_url(timed_video_searches, video_server):
         timed_video_urls = get_images_for_video(timed_video_searches)
 
     return timed_video_urls
+
