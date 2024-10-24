8573f10209402e07464d3a678934cb07887bb418
Adityasorap <114155463+Adityasorap@users.noreply.github.com>
2024-10-24 12:27:55 +0530

    Update background_video_generator.py

--- utility/video/background_video_generator.py
+++ utility/video/background_video_generator.py
@@ -7,11 +7,12 @@ BING_API_KEY = os.environ.get('BING_KEY')
 def search_videos(query_string, orientation_landscape=True):
     url = "https://api.bing.microsoft.com/v7.0/videos/search"
     headers = {
-        "Ocp-Apim-Subscription-Key": BING_API_KEY,
+        "Authorization": BING_API_KEY,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     params = {
-        "q": query_string,
+        "query": query_string,
+        "orientation": "landscape" if orientation_landscape else "portrait",
         "count": 15,
         "mkt": "en-US"
     }
